c13dea2a481a939a8c7d63991c8a1a72adf91fcf
Chris Lewis <chris@chris.to>
2010-11-08 23:16:19 -0800

    Added setup.py, moved README

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description. 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "repositoryhandler",
+    version = "0.3.1",
+    author = "Chris Lewis",
+    author_email = "cflewis@soe.ucsc.edu",
+    description = ("A Python library for accessing source control repositories"),
+    license = "GPL",
+    keywords = "cvs svn git source sourcecontrol scm",
+    url = "https://github.com/Lewisham/repositoryhandler",
+    packages=['repositoryhandler', 'tests'],
+    long_description=read('README.mdown'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Version Control",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Environment :: Console"
+    ],
+)
