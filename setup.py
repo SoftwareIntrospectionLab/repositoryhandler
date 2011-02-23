86e1c5059ca4d639319d24cd2894c56d671b4822
Chris Lewis <chris@chris.to>
2011-02-23 15:09:21 -0800

    Bumped version number for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "repositoryhandler",
-    version = "0.3.1",
+    version = "0.3.2",
     author = "Chris Lewis",
     author_email = "cflewis@soe.ucsc.edu",
     description = ("A Python library for accessing source control repositories"),
